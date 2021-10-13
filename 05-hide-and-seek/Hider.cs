8fa8eebab0dee731e3201b18c2bb888085f58c8f
Linda Spellman <lindaspellman@live.com>
2021-10-12 20:55:54 -0600

    finished hider class

--- 05-hide-and-seek/Hider.cs
+++ 05-hide-and-seek/Hider.cs
@@ -13,13 +13,21 @@ namespace _05_hide_and_seek
     {
         // TODO: Add any member variables here
 
+        public int _location; 
+        public List<int> _distances; 
+
         /// <summary>
         /// Initializes the location of the hider to a random location 1-1000.
         /// Also initializes the list of distances to be a new, empty list.
         /// </summary>
         public Hider()
         {
-            throw new NotImplementedException(); //hello
+            // Start at a new random position from 1-1000
+            Random randomGenerator = new Random();
+            _location = randomGenerator.Next(1, 1001);
+
+            // Set up the list to track the distances we have traveled.            
+            _distances = new List<int>();
         }
 
         /// <summary>
@@ -29,21 +37,52 @@ namespace _05_hide_and_seek
         /// <param name="seekerLocation">The current location of the seeker.</param>
         public void Watch(int seekerLocation)
         {
-            throw new NotImplementedException();
+            // Compute the distance from the seeker
+            int distanceMoved = Math.Abs(_location - seekerLocation);
+            
+            // Add this distance to the end of our list
+            _distances.Add(distanceMoved);
         }
 
         /// <summary>
-        /// Get's a hint.
+        /// Gets a hint.
         /// 
         /// If there is not enough information yet (fewer than 2 distances), a generic hint is given.
         /// If the seeker has found the hider, the hint notes they have been found.
         /// If the seeker is moving closer, the hint notes they are getting warmer.
-        /// If the seeker is moving futher away, the hing notes they are getting colder.
+        /// If the seeker is moving futher away, the hint notes they are getting colder.
         /// </summary>
         /// <returns>The hint message</returns>
         public string GetHint()
         {
-            throw new NotImplementedException();
+            string hint = "";
+            
+            if (_distances.Count < 2)
+            {
+                // we don't have enough information to know if they are getting
+                // warmer or colder, so just give a generic message
+                hint = "(-.-) Maybe I'll take a nap";
+            }
+            else
+            {
+                if (IsFound())
+                {
+                    hint = "(;.;) You found me!";
+                }
+                
+                else if (_distances[_distances.Count - 1] > _distances[_distances.Count - 2])
+                {
+                    // the latest distance is further away than before
+                    hint = "(^.^) Getting colder!";
+                }
+                else
+                {
+                    // The latest distance is the same or closer
+                    hint = "(>.<) Getting warmer!";
+                }
+            }
+
+            return hint;
         }
 
         /// <summary>
@@ -52,7 +91,7 @@ namespace _05_hide_and_seek
         /// <returns>True if the hider has been found.</returns>
         public bool IsFound()
         {
-            throw new NotImplementedException();
+            return _distances[_distances.Count - 1] == 0;
         }
     }
 }
